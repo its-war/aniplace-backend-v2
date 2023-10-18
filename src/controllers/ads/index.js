3f40547e8cb7ab3f7bd3ab4fae1514be9dc05427
Karlos Warney <karloswarney@gmail.com>
2023-10-18 19:39:39 -0300

    ADD - Adicionado rota para o carregamento do banner 300x250px no controller.

--- src/controllers/ads/index.js
+++ src/controllers/ads/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     banner728x90: require('./banner-728x90'),
     banner320x50: require('./banner-320x50'),
+    banner300x250: require('./banner-300x250'),
     adsActive: require('./adsActive')
 }
